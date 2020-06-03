d9df4faf250be7324ae88ae6df049d80ee1b0d6e
anapay <anapay42@gmail.com>
2020-06-03 13:15:52 -0300

    Adicionando construtores(https://github.com/anapay/Orienta-o-a-Objetos.git)

--- 1index.js
+++ 1index.js
@@ -5,34 +5,25 @@ import {Cliente} from "./ACliente.js";
 import {ContaCorrente} from "./BContaCorrente.js";
 
 
-const cliente1 = new Cliente(); //estanciando cliente a classe
-cliente1.nome = "Ana"; //atribuindo valores as propriedades
-cliente1.cpf = 12345678910;
+const cliente1 = new Cliente("Ana", 12345678910); //estanciando cliente a classe
 
-const cliente2 = new Cliente();
-cliente2.nome = "Joao";
-cliente2.cpf = 12345678911;
+const cliente2 = new Cliente("Joao", 12345678911); //passando informações pelo constructor
 
-const conta1 = new ContaCorrente();
+const conta1 = new ContaCorrente(cliente1,1001);
 conta1._saldo = 0;
-conta1.agencia = 13;
-conta1.cliente = cliente1;
-conta1.depositar(102000000);
-conta1.depositar(102000000);
-conta1.depositar(2000);
+// conta1.depositar(102000000);
+// conta1.depositar(102000000);
+// conta1.depositar(2000);
 
-const conta2 = new ContaCorrente();
+const conta2 = new ContaCorrente(cliente2, 1001);
 
 conta2._saldo = 0;
-conta2.agencia = 1001;
-conta2.cliente = cliente2;
-
-conta1.trasferir(2000, conta2);
-conta1.trasferir(1000, conta2);
+// conta1.trasferir(2000, conta2);
+// conta1.trasferir(1000, conta2);
 
 
-console.log(cliente1);
-console.log(conta1);
+console.log(cliente2);
+console.log(conta2);
 
 
 //para criar esse arquivo utilizamos o comando (npm init)
